52bbc22c103db109f136b75ffd27f696ab3bb241
gangadhar <gangadhar@gangadhar-OptiPlex-360.(none)>
2014-08-23 14:33:48 +0530

    for push upstream

--- frappe/utils/email_lib/bulk.py
+++ frappe/utils/email_lib/bulk.py
@@ -149,3 +149,104 @@ def clear_outbox():
 	"""remove mails older than 30 days in Outbox"""
 	frappe.db.sql("""delete from `tabBulk Email` where
 		datediff(now(), creation) > 30""")
+
+
+def multitenanct(from_test=False):
+   res=frappe.db.sql("""select name from `tabSite Master` where flag=0 limit 1 """)
+   if res:
+	sites=''
+	sites = frappe.db.sql("""select sites from  `tabUser` where name='administrator'""")
+	print sites
+	auto_commit = not from_test
+	ste=res[0][0]
+	from frappe.utils import cstr  
+	import os
+	import sys
+	import subprocess
+	import getpass
+	import logging
+	import json
+	from distutils.spawn import find_executable
+	cwd= '/home/gangadhar/workspace/smarttailor/frappe-bench/'
+        cmd='bench new-site '+ste
+	 
+	sites=cstr(sites[0][0])+' '+ste
+	print sites
+	frappe.db.sql("update `tabUser` set sites= %s where name='administrator'",sites)
+        try:
+		subprocess.check_call(cmd, cwd=cwd, shell=True)
+	except subprocess.CalledProcessError, e:
+		print "Error:", e.output
+		raise
+        cmd='bench frappe --install_app erpnext '+ste              
+        try:
+		subprocess.check_call(cmd, cwd=cwd, shell=True)
+	except subprocess.CalledProcessError, e:
+		print "Error:", e.output
+		raise
+        cmd='bench frappe --install_app shopping_cart '+ste             
+        try:
+		subprocess.check_call(cmd, cwd=cwd, shell=True)
+	except subprocess.CalledProcessError, e:
+		print "Error:", e.output
+		raise
+        nginx="""
+		upstream frappe {
+    		server 127.0.0.1:8000 fail_timeout=0;
+		}
+		server {
+			listen 80 ;
+			client_max_body_size 4G;
+			server_name %s;
+			keepalive_timeout 5;
+			sendfile on;
+			root /home/gangadhar/workspace/smarttailor/frappe-bench/sites;
+			location /private/ {
+				internal;
+				try_files /$uri =424;
+			}
+			location /assets {
+				try_files $uri =404;
+			}
+
+			location / {
+				try_files /test/public/$uri @magic;
+			}
+
+			location @magic {
+				proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+				proxy_set_header Host $host;
+				proxy_set_header X-Use-X-Accel-Redirect True;
+				proxy_read_timeout 120;
+				proxy_redirect off;
+				proxy_pass  http://frappe;
+			}
+		}"""%(sites)
+	with open("/home/gangadhar/workspace/smarttailor/frappe-bench/config/nginx.conf","w") as conf_file:
+			conf_file.write(nginx)
+        cwd='/home/'
+        cmd='echo indictrans | sudo service nginx reload'
+        try:
+		subprocess.check_call(cmd, cwd=cwd, shell=True)
+	except subprocess.CalledProcessError, e:
+		print "Error:", e.output
+		raise
+	host="""
+		127.0.0.1       localhost
+		127.0.1.1       gangadhar-OptiPlex-360
+		127.0.0.1       %s
+
+
+		# The following lines are desirable for IPv6 capable hosts
+		::1     ip6-localhost ip6-loopback
+		fe00::0 ip6-localnet
+		ff00::0 ip6-mcastprefix
+		ff02::1 ip6-allnodes
+		ff02::2 ip6-allrouters
+       """%(sites)
+	with open("/home/gangadhar/workspace/smarttailor/frappe-bench/config/hosts","w") as hosts_file:
+			hosts_file.write(host)
+        os.system('echo indictrans | sudo -S cp /home/gangadhar/workspace/smarttailor/frappe-bench/config/hosts /etc/hosts')
+	qry="update `tabSite Master` set flag=1 where name='"+cstr(res[0][0])+"'"
+	frappe.db.sql(qry, auto_commit=auto_commit)
+        
